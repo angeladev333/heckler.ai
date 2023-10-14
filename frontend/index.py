bcb88a042a1a5cd619780d42eff2941a12dd68dc
angelaxudev333 <angela.xu.dev@gmail.com>
2023-10-14 11:46:18 -0400

    finished taipy tutorial

--- frontend/index.py
+++ frontend/index.py
@@ -1,18 +1,17 @@
-from taipy.gui import Gui, notify
+from taipy.gui import Gui, notify, navigate
 
 # Gui(page="# Getting Started\n\nHello, world!").run()  # use_reloader=True
 
 text = "Original Text"
 
 # Definition of the page
-page = """
-<|toggle|theme|>
+page1_md = """
 
 # PresentAR - the modernized presentation assistant
 
 <|layout|columns=1 1|
 <|
-Are you ready to improve your presentation skills?
+## Are you ready to improve your presentation skills?
 |>
 
 <|
@@ -31,6 +30,24 @@ My text: <|{text}|>
 <|Try it out!|button|on_action=on_button_action|>
 """
 
+page2_md = """
+## Rules and Evaluations
+
+We evaluate your presentation skills based on the following
+
+1. Your posture (slouching)
+
+2. Your body language (crossed arms, still gestures, positive gestures like using hand movements)
+
+3. Your facial expressions (smiling vs stoic)
+"""
+
+pages = {
+    "/": "<|toggle|theme|>\n<center>\n<|navbar|>\n</center>",
+    "app": page1_md,
+    "rules": page2_md,
+}
+
 
 def on_button_action(state):
     notify(state, 'info', f'The text is: {state.text}')
@@ -43,4 +60,4 @@ def on_change(state, var_name, var_value):
         return
 
 
-Gui(page).run()
+Gui(pages=pages).run()
