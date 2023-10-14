7f5d6f45ba45dd1ce65b2b1ba41e4f3fb790546a
Miranda Chen <70763804+m-chenie@users.noreply.github.com>
2023-10-14 11:46:44 -0400

    combine gesture and slouch

--- posture.py
+++ posture.py
@@ -19,12 +19,27 @@ def vector_change(frames):
     significant_changes = [i for i, angle in enumerate(angle_changes) if angle > threshold]  # gets index of frame for gesture
     return significant_changes
 
-    # live video
+def isFacingForward(a, b):
+    if ((a < 0 and b > 0) or (a > 0 and b < 0)):
+        # print("FACING DIAGONAL!")
+        return False
+    elif (a > 0 and b > 0) or (a < 0 and b < 0):
+        if abs(a - b) > 0.37:
+            # print("FACING DIAGONAL!")
+            return False
+        else:
+            # print("FACING FORWARD")
+            return True
+
+    return True
+
+# live video
 cap = cv2.VideoCapture(0)  # default webcam
 
 frames = []
 left_wrist_frames = []
 right_wrist_frames = []
+front_slouch_frames = []
 start_time = 0
 
 # set up media pose instance
@@ -56,12 +71,22 @@ with mp_pose.Pose(min_detection_confidence=0.50, min_tracking_confidence=0.5) as
             # visibility: 0.9999997615814209 # % likely of it being visible (basically checking if mediapipe can but a dot on it)
             #print(landmarks)
 
+            # calculate bend of torso/waist
+            # left_knee = [landmarks[25].x, landmarks[25].y]
+            left_waist = [landmarks[23].x, landmarks[23].y]
+            shoulder_left = [landmarks[11].x, landmarks[11].y]
+
+            right_shoulder = [landmarks[12].x, landmarks[12].y]
+            right_waist = [landmarks[24].x, landmarks[24].y]
+            # right_knee = [landmarks[26].x, landmarks[26].y]
+
             wrist_left = [round(landmarks[15].x, 2), round(landmarks[15].y, 2), round(landmarks[15].z, 2)]
             left_wrist_frames.append(wrist_left)
 
             wrist_right = [round(landmarks[16].x, 2), round(landmarks[16].y, 2), round(landmarks[16].z, 2)]
             right_wrist_frames.append(wrist_right)
 
+            # display text on image
             cv2.putText(image, str(wrist_left),
                         tuple(np.multiply([wrist_left[0], wrist_left[1]], [640, 480]).astype(int)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 250), 2, cv2.LINE_AA
@@ -71,6 +96,36 @@ with mp_pose.Pose(min_detection_confidence=0.50, min_tracking_confidence=0.5) as
                         tuple(np.multiply([wrist_right[0], wrist_right[1]], [640, 480]).astype(int)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 250), 2, cv2.LINE_AA
                         )
+
+            shoulder = (landmarks[11].z + landmarks[12].z) / 2  # getting average shoulder z value
+            # print("shoulder =", shoulder)
+
+            waist = (landmarks[23].z + landmarks[24].z) / 2  # getting average waist z value
+            # print("waist =", waist)
+
+            cv2.putText(image, str(shoulder),
+                        tuple(np.multiply([shoulder_left[0], shoulder_left[1]], [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 250), 2, cv2.LINE_AA
+                        )
+
+            cv2.putText(image, str(waist),
+                        tuple(np.multiply([left_waist[0], left_waist[1]], [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 250), 2, cv2.LINE_AA
+                        )
+
+            if isFacingForward(landmarks[12].z, landmarks[11].z):
+                shoulder = (landmarks[11].z + landmarks[12].z) / 2
+                print("shoulder =", shoulder)
+
+                waist = (landmarks[23].z + landmarks[24].z) / 2
+                print("waist =", waist)
+
+                knee = (landmarks[25].z + landmarks[26].z) / 2
+                print("knee =", knee)
+
+                if abs(shoulder) / abs(waist) > 750:
+                    print("shoulders slouched! SLOUCHER! FOUND THE SLOUCHER!")
+                    front_slouch_frames.append(frame)
 
         except:
             pass
@@ -105,10 +160,10 @@ print(gesture_index)
 # list of a frame index where there are no gestures for a long period of time
 absent_gesture = []
 
-# i = index of gesture in frame array (aka frame number)
-for i in gesture_index:
-    cv2.imshow("asdf", frames[i])
-    cv2.waitKey(0)
+# # i = index of gesture in frame array (aka frame number)
+# for i in gesture_index:
+#     cv2.imshow("asdf", frames[i])
+#     cv2.waitKey(0)
 
 # finding where are no gestures
 if len(gesture_index) == 1 and elapse_time > 13:
@@ -121,6 +176,10 @@ else:
 
 for i in absent_gesture:
     cv2.imshow("no gesture", frames[i])
+    cv2.waitKey(0)
+
+for f in front_slouch_frames:
+    cv2.imshow("shouching", f)
     cv2.waitKey(0)
 
 cv2.waitKey(0)
