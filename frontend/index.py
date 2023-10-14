6f16c9742a722a0283877b3011bf3003114d39e1
angelaxudev333 <angela.xu.dev@gmail.com>
2023-10-14 17:51:01 -0400

    help

--- frontend/index.py
+++ frontend/index.py
@@ -1,17 +1,40 @@
-from taipy.gui import Gui, notify, navigate
+from taipy.gui import Gui, notify
+from webcam import Webcam
+import cv2
 
-# Gui(page="# Getting Started\n\nHello, world!").run()  # use_reloader=True
+import PIL.Image
+import io
 
-text = "Original Text"
+import logging
+import uuid
+from pathlib import Path
+from demo.faces import detect_faces, recognize_face, train_face_recognizer
+
+logging.basicConfig(level=logging.DEBUG)
+
+training_data_folder = Path("images")
+
+show_capture_dialog = False
+capture_image = False
+show_add_captured_images_dialog = False
+
+labeled_faces = []  # Contains rect with label (for UI component)
+
+captured_image = None
+captured_label = ""
+
 
 # Definition of the page
-page1_md = """
+page = """
+<|toggle|theme|>
 
-# PresentAR - the modernized presentation assistant
+# **PresentAR**{: .color-primary} - the modernized presentation assistant
+
+Welcome to PresentAR! We use [Taipy](https://taipy.io/) with a [custom GUI component](https://docs.taipy.io/en/latest/manuals/gui/extension/) to capture video from your webcam and do realtime presentation feedback 
 
 <|layout|columns=1 1|
 <|
-## Are you ready to improve your presentation skills?
+Are you ready to improve your presentation skills?
 |>
 
 <|
@@ -23,12 +46,16 @@ page1_md = """
 |>
 |>
 
-My text: <|{text}|>
+<|card|part|
+## **Webcam**{: .color-primary} component
 
-<|{text}|input|>
-
-<|Try it out!|button|on_action=on_button_action|>
+<|text-center|part|
+<|webcam.Webcam|id=my_webcam|sampling_rate=100|>
+>
+|card>
+|container>
 """
+
 
 page2_md = """
 ## Rules and Evaluations
@@ -49,15 +76,24 @@ pages = {
 }
 
 
-def on_button_action(state):
-    notify(state, 'info', f'The text is: {state.text}')
-    state.text = "Button Pressed"
+# def on_button_action(state):
+#     notify(state, 'info', f'The text is: {state.text}')
+#     state.text = "Button Pressed"
 
 
-def on_change(state, var_name, var_value):
-    if var_name == "text" and var_value == "Reset":
-        state.text = ""
-        return
+# def on_change(state, var_name, var_value):
+#     if var_name == "text" and var_value == "Reset":
+#         state.text = ""
+#         return
 
 
-Gui(pages=pages).run()
+if __name__ == "__main__":
+    # # Create dir where the pictures will be stored
+    # if not training_data_folder.exists():
+    #     training_data_folder.mkdir()
+
+    # train_face_recognizer(training_data_folder)
+
+    gui = Gui(pages=pages)
+    gui.add_library(Webcam())
+    gui.run()
